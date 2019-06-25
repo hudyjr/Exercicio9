975eab866057f12171c564a42db1ea507a516b51
hudyjr <48738196+hudyjr@users.noreply.github.com>
2019-06-24 21:26:15 -0300

    Add files via upload

---
+++ Quest1/arara.java
@@ -0,0 +1,14 @@
+package Quest1;
+
+public class arara extends ave{
+    
+    public arara(String n, boolean v){
+    
+        super(n, v);
+        
+    }
+    public void talk(){
+    
+        System.out.println("Arara, Arara");
+    }
+}
